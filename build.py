4ec73dac1502afa6e11b8babc2340c905738b3f4
David Francoeur <dfrancoeur04@gmail.com>
2017-05-29 15:05:15 -0400

    chore(misc): eslint, npm, git

---
+++ build.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python
+from jinja2 import Template, Environment, FileSystemLoader
+import os
+
+# Capture our current directory
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def render_templates():
+    j2_env = Environment(
+        loader=FileSystemLoader(THIS_DIR),
+        trim_blocks=True
+    )
+
+    context = {
+        "jsrequire": j2_env.get_template("require.js.j2").render(),
+        "jsauth": j2_env.get_template("auth.js.j2").render()
+    }
+
+    files = [
+        "post-events.js",
+        "create-object.js"
+    ]
+    for file in files:
+        filename = THIS_DIR + "/dist/" + file
+        print filename
+        with open(filename, 'w') as f:
+            f.write(j2_env.get_template(file + '.j2').render(context))
+
+if __name__ == '__main__':
+    render_templates()
